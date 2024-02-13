faac6032d7c90d1edc38b79deb93ab63d4246bb5
Sanya <Sanya-dmb@yandex.ru>
2024-02-13 18:39:45 +0700

    add task3

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число из отрезка [10, 99]: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
+Console.WriteLine(maxDigit);
