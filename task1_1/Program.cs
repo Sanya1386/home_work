0e6e0d2761a42b69303063ad7d1b8dcceca7f838
Sanya <Sanya-dmb@yandex.ru>
2024-02-14 20:10:20 +0700

    add task1_1:task1_2

---
+++ task1_1/Program.cs
@@ -0,0 +1,10 @@
+﻿    int[] numbers = new int[] { 10, 2, 32, 4, 11, 6, 7, 51, 15, 10 };
+    // 1 вариант
+    int count = 0;
+    foreach (int number in numbers)
+        if (number >= 10 && number <= 90)
+            count++;
+ 
+    Console.WriteLine (" от 10 до 90: " + count);
+    // 2 вариант
+    // Console.WriteLine ($" от 10 до 90: {numbers.Where (n => (n >= 10 && n <= 90)).Count()}");
---
+++ task1_2/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = 10;
+int[] array = { 11, 15, 21, 45, 80, 32, 11, 60, 75, 88 };
+int i = 0;
+int SumEven = 0;
+
+while (i < n)
+{
+    if (array[i] % 2 == 0)
+        SumEven++;
+        i = i +1;
+}
+System.Console.WriteLine(SumEven);
